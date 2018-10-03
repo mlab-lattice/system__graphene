57ba0f4e200210d6db962d5718a0ad7c32f3a257
Ben Elgar <me@benelgar.com>
2018-10-03 20:05:44 +0200

    Add scripts for managing infrastructure

---
+++ provision-lattice-infrastructure.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o pipefail
+
+if [ -z "$AWS_ACCESS_KEY_ID" ] && [ -z "$AWS_SECRET_ACCESS_KEY" ]; then
+  echo 'AWS creds not set. Please set the "AWS_ACCESS_KEY_ID" and "AWS_SECRET_ACCESS_KEY" environment variables.'
+  exit 1
+fi
+
+# Do this after checking for AWS creds to give good error message.
+set -o nounset
+
+IMAGE_URI=161545394894.dkr.ecr.us-west-2.amazonaws.com/bundled-k8s-util-linux
+
+docker pull $IMAGE_URI
+
+docker run -e AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY $IMAGE_URI:latest create \
+  --id lattice-staging-app \
+  --container-channel gcr.io/lattice-dev/laas/staging \
+  --cloud-provider AWS \
+  --api-auth-key $(cat ./infrastructure-bearer-token.txt) \
+  --cloud-provider-var=master-node-instance-type=t2.medium \
+  --cloud-provider-var=account-id=161545394894 \
+  --cloud-provider-var=key-name=lattice-kp-us-east-2 \
+  --cloud-provider-var=region=us-east-2 \
+  --cloud-provider-var=availability-zones=us-east-2a,us-east-2b,us-east-2c \
+  --cloud-provider-var=terraform-backend-s3-bucket=laas.dev.staging \
+  --cloud-provider-var=terraform-backend-s3-key=lattices.lattice-staging-app/terraform/terraform.tfstate \
+  --cloud-provider-var=terraform-backend-s3-bucket-region=us-east-1
