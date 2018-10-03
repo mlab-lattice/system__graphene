113538724b4f2aae3a665ff1e16adf40c4d50121
Ben Elgar <me@benelgar.com>
2018-10-03 19:37:37 +0200

    Use binary from resource rather than S3

--- ci/deploy-new-lattice-system.sh
+++ ci/deploy-new-lattice-system.sh
@@ -20,9 +20,7 @@ cat <<EOT >> config.json
 }
 EOT
 
-# FIXME: do not get from S3 but from a resource
-wget https://s3-us-east-2.amazonaws.com/lattice-binaries-staging/latticectl_linux_amd64_vv0.1.1-91-gcfdfa70 -O latticectl
-
+cp linux-latticectl-binary/latticectl_* latticectl
 chmod +x latticectl
 
 ./latticectl build --system infrastructure --version $(cat ./system-definition-repo/.git/ref) --config config.json
